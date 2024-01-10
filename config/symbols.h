3bff8d5744d611f414f4e73b26964babdae49bab
pro_baller3000 <floennek@gmx.de>
2024-01-10 23:27:49 +0100

    pipe

--- config/symbols.h
+++ config/symbols.h
@@ -381,7 +381,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define DE_HASH  BSLH          // #
 // #define DE_TILDE  LA(RBKT)     // ~
 // #define DE_PIPE  RA(GRAVE)     // "|"
-#define DE_PIPE  RA(TILDE)     // "|"
+#define DE_PIPE  RA(NUBS)     // "|"
 // #define DE_AMPS  CARET         // "&" DE_AMPR
 #define DE_BSLH  RA(MINUS)     // "\" DE_BSLS
 // #define DE_QUESTION  LS(MINUS) // ? DE_QUES
